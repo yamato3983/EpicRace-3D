53a97db10f210696a115853dbf87e8f67308099b
HI04 <rocksun410@gmail.com>
2021-05-17 09:16:55 +0900

    Revert "リスポーン作業途中経過"
    This reverts commit 4e6cac522bc2bb186553c9c9d4970e7f55eb52eb.

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -4,56 +4,17 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject RP;
-
-   [SerializeField]
+    [SerializeField]
     GameObject GoalLine_PL;	// 移動予定地のオブジェクト
     //public Transform target = GameObject.Find("GoalLine_PL").transform;
     NavMeshAgent agent;
     int flg = 0;
-    Vector3 tmp;
-    Rigidbody rb;
-
-    public bool Dead = false;
-
-    //[SerializeField] NavMeshAgent nav_mesh_agent;
-    //public bool Dead = false;
-
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-
-        RP = GameObject.Find("RespawnPoint");
-        tmp = RP.transform.position;
-        
+        //Vector3 tmp = GameObject.Find("Player").transform.position;
+        //Debug.Log(tmp);
         agent = GetComponent<NavMeshAgent>();
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        var agentRigidbody = agent.GetComponent<Rigidbody>();
-
-        if (other.gameObject.tag == "Dead")
-        {
-            //ナビゲーションを止める
-            NavMeshAgent agent = GetComponent<NavMeshAgent>();
-           agent.isStopped = true;
-
-            agent.Warp(new Vector3(tmp.x, tmp.y, tmp.z));
-        }
-        if (other.tag == "Respawn")
-        {
-            Debug.Log("触れた！！");
-            //agent.updatePosition = false;
-        }
-
-        if (other.tag == "Goal")
-        {
-            NavMeshAgent agent = GetComponent<NavMeshAgent>();
-            agent.isStopped = true;
-            Debug.Log("ゴーーール");
-        }
     }
 
     void Update()
@@ -75,6 +36,5 @@ public class PlayerController : MonoBehaviour
         {
             agent.GetComponent<NavMeshAgent>().isStopped = true;
         }
-
     }
 }
