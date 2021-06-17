0958cfb95a080a021a73c0d178c46a76c4ca42ed
bontenmarukamisama <bontenmarukamisama@gmail.com>
2021-06-17 11:49:28 +0900

    NPC側のリザルト画面の細かい修正とプレイヤーがゴールした際にリザルト画面にいく

---
+++ Assets/Script/Enemy/playergoal/P_Goal01.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;//シーン切り替えに使用するライブラリ
+
+public class P_Goal01 : MonoBehaviour
+{
+    //NPCのスクリプト
+    GameObject unitychan;
+    public PlayerController script_p01;
+
+    public bool stage01;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stage01 = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        unitychan = GameObject.Find("unitychan");
+        script_p01 = unitychan.GetComponent<PlayerController>();
+
+        //NPCがゴールしたらシーンを変更する
+        if (script_p01.Gflg == true)
+        {
+            stage01 = true;
+            SceneManager.LoadScene("Clear_player", LoadSceneMode.Single);
+        }
+    }
+}
---
+++ Assets/Script/Scenen/Pleyer_clear/Player_clear02.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_clear02 : MonoBehaviour
+{
+
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("urabe03");
+    }
+
+}
