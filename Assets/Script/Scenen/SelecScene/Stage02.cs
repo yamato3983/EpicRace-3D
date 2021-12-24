36eec2ab8efd88bc92eb2c1ab7016874d35e6ef7
bontenmarukamisama <bontenmarukamisama@gmail.com>
2021-12-24 11:14:59 +0900

    ステージ選択とプレイヤーがゴールまでのUIを作成

---
+++ Assets/Script/Scenen/SelecScene/Stage02.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage02 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("Stage02");
+    }
+}
--- Assets/Script/Scenen/Title/StartButton.cs
+++ Assets/Script/Scenen/Title/StartButton.cs
@@ -29,7 +29,7 @@ public class StartButton : MonoBehaviour
 
         //SceneManager.LoadScene("Level");
 
-          SceneManager.LoadScene("Stage01");
+          SceneManager.LoadScene("SelectStage");
         }
 
     }
